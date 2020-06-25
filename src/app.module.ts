1d77f5e79768a5c8f14a2d32670f4aaa14587e1d
Maduri <Manoj.Maduri@McKesson.com>
2020-06-25 12:37:50 -0400

    created new tweet service && ctrl

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TweetsService } from './tweets/tweets.service';
+import { TweetsController } from './tweets/tweets.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TweetsController],
+  providers: [AppService, TweetsService],
 })
 export class AppModule {}
