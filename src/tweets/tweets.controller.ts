360731672a7d2b0f9f08c071239b35e946431e11
Maduri <Manoj.Maduri@McKesson.com>
2020-06-25 14:19:57 -0400

    we now have create/getAll/getById calls ready in our nestjs app

--- src/tweets/tweets.controller.ts
+++ src/tweets/tweets.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { TweetsService } from './tweets.service';
+
+interface Tweet {
+    id: string,
+    name: string
+}
 
 @Controller('tweets')
-export class TweetsController {}
+export class TweetsController {
+    constructor(private readonly tweetService: TweetsService){}
+
+    @Get()
+    getTweets() {
+      return this.tweetService.getTweet();
+    }
+
+    @Get(':id')
+    getTweet(@Param() Params) {
+      return this.tweetService.getTweet().filter(p => p.id == Params.id);
+    }
+
+    @Post()
+    createTweet(@Body() tweet: Tweet) {
+     this.tweetService.createTweet(tweet);
+    }
+
+
+}
--- src/tweets/tweets.service.ts
+++ src/tweets/tweets.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TweetsService {}
+export class TweetsService {
+    tweets = [{
+        id: 1,
+        name: 'A SPA app'
+    },
+    {
+        id: 2,
+        name: 'A Nest API'
+    }];
+
+    getTweet() {
+        return this.tweets;
+    }
+
+    createTweet(data) {
+        this.tweets = [...this.tweets, { ...data }];
+    }
+}
